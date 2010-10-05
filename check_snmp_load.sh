3c4931d71a3d66f39016aa5fed928529311809f1
Leon Waldman <le.waldman@gmail.com>
2010-10-05 11:39:28 +0200

    Version 0.2 - Added changes made by Suttoni and a changelog file.

--- check_snmp_load.sh
+++ check_snmp_load.sh
@@ -1,112 +1,127 @@
 #!/bin/bash
+
 ###
-# Writed by Leon Waldman (le.waldman at gmail.com)
-# Any Improvement ( And please, if you can, improve it!:) ) please send a copy to me! ;)
+# Written by Leon Waldman (le.waldman at gmail.com)
+# This script is freely available on Github: http://github.com/waldman/Nagios-Check-SNMP-Load
+# v0.2
 ###
 
 # Exit codes:
-# 0	Ok
-# 1	Warning
-# 2	Critical
-# 3	Unknow
+# 0 OK
+# 1 WARNING
+# 2 CRITICAL
+# 3 UNKNOWN
+
 
 # How to use this Script?
 usage(){
     echo "Usage:"
     echo " $0 -H <host> -w WLOAD1,WLOAD5,WLOAD15 -c CLOAD1,CLOAD5,CLOAD15 [-C snmp_community]"
     echo " $0 -h help"
-} 
+}
 
-# ERRORr control 
+
+# ERROR control 
 ERROR=0
+
 
 # Getting Options
 while getopts C:H:w:c:h option; do
     case $option in
     H)
-	HOST=$OPTARG
+ HOST=$OPTARG
     ;;
     C)
-	COMUNITY=$OPTARG
+ COMUNITY=$OPTARG
     ;;
     w)
-	WARNING=$OPTARG
+ WARNING=$OPTARG
     ;;
     c)
-	CRITICAL=$OPTARG
+ CRITICAL=$OPTARG
     ;;
     h)
-	usage
-	ERROR=1
+ usage
+ ERROR=1
     ;;
     *)
-	usage
-	ERROR=1
+ usage
+ ERROR=1
     ;;
     esac
 done
+
+
 # Checking Parameters
-TESTW="echo $WARNING| grep -o ","| wc -l"
-TESTC="echo $CRITICAL| grep -o ","| wc -l"
+TESTW="`echo $WARNING| grep -o ','| wc -l`"
+TESTC="`echo $CRITICAL| grep -o ','| wc -l`"
 if [ $TESTW -ne 2 ]; then
     ERROR=1
 fi
-
 if [ $TESTC -ne 2 ]; then
     ERROR=1
 fi
-
 if [ -z "$HOST" ]; then
     ERROR=1
 fi
-
 if [ -z "$COMUNITY" ]; then
-    ERROR=1
-fi
-
-if [ $ERROR -ne 0 ]; then
-    usage
     exit 3
 fi
+
 
 ### Party Start!
-# Getting the load from server thru SNMP
-LOAD=`snmpwalk -v 1 -c $COMUNITY $HOST .1.3.6.1.4.1.2021.10.1.3|awk '{ print $4 }'`
+# Getting the load from server through SNMP
+LOAD=`snmpwalk -v 1 -c $COMUNITY $HOST UCD-SNMP-MIB::laLoad 2>&1|awk '{ print $4 }'`
 CHECK=`echo $LOAD|grep -o "\."|wc -l`
-if [ $CHECK -ne 3 ]; then
-    echo "UNKNOW - snmpwalk output malformed."
+if [[ "${LOAD}" =~ "from" ]]
+then
+    echo "UNKNOWN - Timeout: No Response from $HOST"
+    exit 3
+elif [ $CHECK -ne 3 ]; then
+    echo "UNKNOWN - snmpwalk output malformed."
     exit 3
 fi
 
-# Getting Values Load
-L1=`echo $LOAD|awk '{ print $1 }'`
-L2=`echo $LOAD|awk '{ print $2 }'`
-L3=`echo $LOAD|awk '{ print $3 }'`
 
-LC1=`echo $LOAD|awk '{ print $1 }'|sed 's/\.//g'`
-LC2=`echo $LOAD|awk '{ print $2 }'|sed 's/\.//g'`
-LC3=`echo $LOAD|awk '{ print $3 }'|sed 's/\.//g'`
-
-# Getting Values Warning
-W1=`echo $WARNING|awk -F , '{ print $1 }'|sed 's/\.//g'`
-W2=`echo $WARNING|awk -F , '{ print $2 }'|sed 's/\.//g'`
-W3=`echo $WARNING|awk -F , '{ print $3 }'|sed 's/\.//g'`
-
-# Getting Critical Values
-C1=`echo $CRITICAL|awk -F , '{ print $1 }'|sed 's/\.//g'`
-C2=`echo $CRITICAL|awk -F , '{ print $2 }'|sed 's/\.//g'`
-C3=`echo $CRITICAL|awk -F , '{ print $3 }'|sed 's/\.//g'`
-
-# Faz a Magica!!
-if [[ $LC1 -gt $C1  ]] || [[ $LC2 -gt $C2  ]] || [[ $LC3 -gt $C3  ]]; then
-    echo "LOAD CRITICAL - load average: $L1 $L2 $L3"
-    exit 2
-elif [[ $LC1 -gt $W1  ]] || [[ $LC2 -gt $W2  ]] || [[ $LC3 -gt $W3  ]]; then
-    echo "LOAD WARNING - load average: $L1 $L2 $L3"
-    exit 1
-else
-    echo "LOAD OK - load average: $L1 $L2 $L3"
-    exit 0
-fi
-
+# print statements commented out but ready to enable for debugging
+echo $WARNING | awk 'BEGIN{
+ while ( "snmpwalk -v 1 -c '$COMUNITY' '$HOST' UCD-SNMP-MIB::laLoad" |getline )
+ serverload[++i] = $NF
+# print serverload[1]
+# print serverload[2]
+# print serverload[3]
+}
+{
+ w=split(warning,warnval,",")
+ c=split(critical,critval,",")
+# print warnval[1]
+# print warnval[2]
+# print warnval[3]
+# print critval[1]
+# print critval[2]
+# print critval[3]
+if( serverload[1] > critval[1] ) { 
+ print "Load : " serverload[1] " " serverload[2] " " serverload[3] " : " serverload[1] " > " critval[1] " : CRITICAL"; exit 2 
+}
+else if( serverload[2] > critval[2] ) { 
+ print "Load : " serverload[1] " " serverload[2] " " serverload[3] " : " serverload[2] " > " critval[2] " : CRITICAL"; exit 2 
+}
+else if( serverload[3] > critval[3] ) { 
+ print "Load : " serverload[1] " " serverload[2] " " serverload[3] " : " serverload[3] " > " critval[3] " : CRITICAL"; exit 2 
+}
+else if( serverload[1] > warnval[1] ) { 
+ print "Load : " serverload[1] " " serverload[2] " " serverload[3] " : " serverload[3] " > " warnval[3] " : WARNING"; exit 1 
+}
+else if( serverload[2] > warnval[2] ) { 
+ print "Load : " serverload[1] " " serverload[2] " " serverload[3] " : " serverload[3] " > " warnval[3] " : WARNING"; exit 1 
+}
+else if( serverload[3] > warnval[3] ) { 
+ print "Load : " serverload[1] " " serverload[2] " " serverload[3] " : " serverload[3] " > " warnval[3] " : WARNING"; exit 1 
+}
+else {
+ print "Load : " serverload[1] " " serverload[2] " " serverload[3] " : OK"; exit 0
+}
+}
+END { exit }' warning=$WARNING critical=$CRITICAL
+exit $?
 ### Party Over!
